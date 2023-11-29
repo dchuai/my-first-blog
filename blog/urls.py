5a3257e181bf632c794bfd0e812e7491fb662bc5
dchuai <128651299+dchuai@users.noreply.github.com>
2023-11-28 21:18:08 -0500

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# blog/urls.py
+
+from django.urls import path
+from . import views  # Import views from the same directory
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),  # Define the URL pattern
+]
